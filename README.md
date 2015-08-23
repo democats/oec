bf27c1b3600a051246e48a1516696e81b4b22ace
Petar Mitchev <petar@imeleon.com>
2015-08-23 12:22:17 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# oec
-Oneevilcoin pool frontend
---
+++ config.js
@@ -0,0 +1,16 @@
+var api = "http://pool-api.democats.org:7623";
+
+var poolHost = "pool-api.democats.org";
+
+var irc = "irc.freenode.net/#democats";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/ease-way-light-theme.css";
+
